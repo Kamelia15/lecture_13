b19973920cf8b146f236926c0297fa8074dfa682
Kamilya Sadriyeva <xsadri00@vutbr.cz>
2021-05-03 10:21:36 +0200

    rec_f_p

--- fibo.py
+++ fibo.py
@@ -1,7 +1,15 @@
+def recursive_nth_fibo(n):
+    if n in (0,1):
+        return 1
+    else:
+        return (recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2))
+
+
+
 
 def main():
-    pass
-
+    fib = recursive_nth_fibo(8)
+    print(fib)
 
 if __name__ == '__main__':
     main()
